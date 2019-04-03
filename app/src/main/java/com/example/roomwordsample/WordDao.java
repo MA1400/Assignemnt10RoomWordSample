be495fb56b0673dc683921ef9410cdd6a3e286a1
ma1400 <malwan1@css.edu>
2019-04-03 16:58:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/WordDao.java
@@ -0,0 +1,12 @@
+package com.example.roomwordsample;
+@Dao
+public interface WordDao {
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
---
+++ app/src/main/java/com/example/roomwordsample/WordViewModel.java
@@ -0,0 +1,24 @@
+package com.example.roomwordsample;
+
+import android.app.Application;
+
+import java.util.List;
+
+import AndroidViewModel;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private WordRepository mRepository;
+
+    private LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
